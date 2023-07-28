25dc4463359d1b953ec5ae98f8dfe5e5bff095cf
JulianIrigoyen <julian.irigoyen@experoinc.com>
2023-07-28 11:44:30 +0200

    Shkol init: Fixed migrations. Deployed contract V0 <3

--- contracts/Shkolz.sol
+++ contracts/Shkolz.sol
@@ -1,17 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {ShkolOwnership} from "./ShkolOwnership.sol";
+
 contract Shkolz {
-    // Event to be emitted upon receiving Ether
-    event Received(address sender, uint amount);
-
-    // Fallback function to accept Ether. This function is called when Ether is sent directly to the contract.
-    receive() external payable {
-        emit Received(msg.sender, msg.value);  // Emit event with sender address and received amount
-    }
-
-    // Function to check the balance of the contract
-    function getBalance() public view returns (uint) {
-        return address(this).balance;
-    }
 }
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,21 @@
+// Import all the contracts
+const Ownable = artifacts.require("Ownable");
+const ShkolFactory = artifacts.require("ShkolFactory");
+const ShkolHelper = artifacts.require("ShkolHelper");
+const ERC721 = artifacts.require("ERC721");
+const ShkolOwnership = artifacts.require("ShkolOwnership");
+const Shkolz = artifacts.require("Shkolz");
+const PoapInterface = artifacts.require("PoapInterface");
+const ShkolDevelopment = artifacts.require("ShkolDevelopment");
+
+module.exports = function(deployer) {
+  // Deploy the contracts in the correct order
+  deployer.deploy(ShkolFactory)
+    .then(() => deployer.deploy(ShkolHelper))
+    .then(() => deployer.deploy(PoapInterface))
+    .then(() => deployer.deploy(ShkolDevelopment)
+    .then(() => deployer.deploy(ERC721, "Shkol", "SKL"))
+    .then(() => deployer.deploy(ShkolOwnership))
+    .then(() => deployer.deploy(Shkolz))
+    );
+};
